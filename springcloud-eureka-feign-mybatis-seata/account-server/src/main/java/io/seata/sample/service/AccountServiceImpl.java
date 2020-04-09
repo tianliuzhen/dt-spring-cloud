fd45f3cfdb79a64ae0050457693f249f6c1a6ef3
tianliuzhen <tianliuzhen@haoxiaec.com>
2020-04-09 11:47:26 +0800

    完善sql

--- springcloud-eureka-feign-mybatis-seata/account-server/src/main/java/io/seata/sample/service/AccountServiceImpl.java
+++ springcloud-eureka-feign-mybatis-seata/account-server/src/main/java/io/seata/sample/service/AccountServiceImpl.java
@@ -3,6 +3,8 @@ package io.seata.sample.service;
 import io.seata.sample.dao.AccountDao;
 import io.seata.sample.feign.OrderApi;
 import java.math.BigDecimal;
+
+import io.seata.spring.annotation.GlobalTransactional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,6 +28,7 @@ public class AccountServiceImpl implements AccountService{
      * @param money 金额
      */
     @Override
+    @GlobalTransactional(name = "fsp-update-order",rollbackFor = Exception.class)
     public void decrease(Long userId, BigDecimal money) {
         LOGGER.info("------->扣减账户开始account中");
         //模拟超时异常，全局事务回滚
@@ -39,7 +42,11 @@ public class AccountServiceImpl implements AccountService{
 
         //修改订单状态，此调用会导致调用成环
         LOGGER.info("修改订单状态开始");
-        String mes = orderApi.update(userId, money.multiply(new BigDecimal("0.09")),0);
+        String mes = orderApi.update(userId, money.multiply(new BigDecimal("0.9")),0);
         LOGGER.info("修改订单状态结束：{}",mes);
+
+        if (true){
+            throw new RuntimeException("自定义抛异常");
+        }
     }
 }
