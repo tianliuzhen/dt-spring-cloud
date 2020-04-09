cc22c44ed3cca01ea308f3bac8f2a18f51e889c4
tianliuzhen <tianliuzhen@haoxiaec.com>
2020-04-09 18:17:44 +0800

    yml 的 形式替代 file.conf 和 resity.conf

---
+++ my-dt-eureka-seata/provider-server/src/main/java/com/aaa/providerserver/service/AccountServiceImpl.java
@@ -0,0 +1,52 @@
+package com.aaa.providerserver.service;
+
+import com.aaa.providerserver.dao.AccountDao;
+import com.aaa.providerserver.feign.OrderApi;
+import io.seata.spring.annotation.GlobalTransactional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+/**
+ * @author IT云清
+ */
+@Service("accountServiceImpl")
+public class AccountServiceImpl implements AccountService{
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AccountServiceImpl.class);
+    @Autowired
+    private AccountDao accountDao;
+    @Autowired
+    private OrderApi orderApi;
+
+    /**
+     * 扣减账户余额
+     * @param userId 用户id
+     * @param money 金额
+     */
+    @Override
+    @GlobalTransactional(name = "fsp-update-order",rollbackFor = Exception.class)
+    public void decrease(Long userId, BigDecimal money) {
+        LOGGER.info("------->扣减账户开始account中");
+        //模拟超时异常，全局事务回滚
+//        try {
+//            Thread.sleep(30*1000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+        accountDao.decrease(userId,money);
+        LOGGER.info("------->扣减账户结束account中");
+
+        //修改订单状态，此调用会导致调用成环
+        LOGGER.info("修改订单状态开始");
+        String mes = orderApi.update(userId, money.multiply(new BigDecimal("0.9")),0);
+        LOGGER.info("修改订单状态结束：{}",mes);
+
+        if (true){
+            throw new RuntimeException("自定义抛异常");
+        }
+    }
+}
